dabbb9ab8bd0a367bd3ecbc5d3f55b629db460f5
malanak <malanakgames@seznam.cz>
2025-11-11 11:44:20 +0100

    fix typo in BuildDocker.sh

--- BuildDocker.sh
+++ BuildDocker.sh
@@ -1,5 +1,5 @@
 docker build -t hackathonapifin:latest .
-cd deps/hackaton_pdf_to_txt/pdf-to-text || exit
+cd deps/hackaton_pdf_to_txt/ || exit
 docker build -t pdf_to_text:latest .
 cd ../../deps/hackaton_text_to_json/ || exit
 docker build -t text_to_json:latest .
